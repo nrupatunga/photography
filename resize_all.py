251126f66f1ee06225bf0c0cb65f3766d410d9d6
nrupatunga <nrupatunga.tunga@gmail.com>
2020-10-30 22:12:02 +0530

    generate thumbnails using python script

---
+++ resize_all.py
@@ -0,0 +1,21 @@
+"""
+File: resize_all.py
+Author: Nrupatunga
+Email: nrupatunga.s@byjus.com
+Github: https://github.com/nrupatunga
+Description: resize images
+"""
+import cv2
+import imutils
+from imutils import paths
+from pathlib import Path
+
+
+fr_dir = './images/fulls'
+lr_dir = './images/thumbs'
+
+for img_path in paths.list_images(fr_dir):
+    out_path = Path(lr_dir).joinpath(img_path.split('/')[-1])
+    img = cv2.imread(img_path)
+    img = imutils.resize(img, width=512)
+    cv2.imwrite(str(out_path), img)
