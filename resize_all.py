c15ad7860f101ebc0693a29e216364437da32517
nrupatunga <nrupatunga.tunga@gmail.com>
2020-10-30 22:23:02 +0530

    rename

--- resize_all.py
+++ resize_all.py
@@ -15,7 +15,9 @@ fr_dir = './images/fulls'
 lr_dir = './images/thumbs'
 
 for img_path in paths.list_images(fr_dir):
+    img_path.replace('JPG', 'jpg')
     out_path = Path(lr_dir).joinpath(img_path.split('/')[-1])
     img = cv2.imread(img_path)
     img = imutils.resize(img, width=512)
     cv2.imwrite(str(out_path), img)
+    cv2.imwrite(img_path, img)
